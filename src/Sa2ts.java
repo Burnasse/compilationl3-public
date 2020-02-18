fd61b59fa13318e26bd2cd39cf3de95ded58378b
l17004536 <dontstopmenow_12>
2020-02-18 13:40:02 +0100

    fin tp3,4 suite

--- src/Sa2ts.java
+++ src/Sa2ts.java
@@ -3,7 +3,7 @@ import ts.Ts;
 import ts.TsItemFct;
 import ts.TsItemVar;
 
-public class Sa2ts extends SaDepthFirstVisitor <Void> {
+public class Sa2ts extends SaDepthFirstVisitor<Void> {
     /*
     Hash ->
             fonctions -> noms variables | TsItemVar
@@ -17,87 +17,96 @@ public class Sa2ts extends SaDepthFirstVisitor <Void> {
 
     Ts table = new Ts();
 
+    public Sa2ts(SaNode saRoot) {
+        saRoot.accept(this);
+    }
+
     @Override
     public Void visit(SaDecTab node) {
-        TsItemVar item = node.tsItem;
+        System.out.println("1");
 
-        if(item.isParam || table.variables.containsKey(item.identif))
-            return null;
+        if (/*item.isParam  || */table.variables.containsKey(node.getNom()))
+            return super.visit(node);
 
-        table.addVar(item.identif,item.taille);
+        table.addVar(node.getNom(), node.getTaille());
 
         return super.visit(node);
     }
 
     @Override
     public Void visit(SaDecFonc node) {
-        TsItemFct item = node.tsItem;
 
-        if(table.fonctions.containsKey(item.identif))
-            return null;
-        table.addFct(item.identif,item.nbArgs,item.getTable(),item.saDecFonc);
+        if (table.fonctions.containsKey(node.getNom()))
+            return super.visit(node);
+
+        table.addFct(node.getNom(), 1, table, node);
+
         return super.visit(node);
     }
 
     @Override
     public Void visit(SaDecVar node) {
-        TsItemVar item = node.tsItem;
-        if(item.portee.variables.containsKey(item.identif))
-            return null;
+        System.out.println("2");
+        if (table.variables.containsKey(node.getNom()))
+            return super.visit(node);
 
-        table.addVar(item.identif,item.taille);
+        table.addVar(node.getNom(), 1);
         return super.visit(node);
     }
 
     @Override
     public Void visit(SaVarSimple node) {
-        TsItemVar item = node.tsItem;
+        System.out.println("3");
 
-        if(item.portee.variables.containsKey(item.identif))
-           return super.visit(node);
+        if (table.variables.containsKey(node.getNom()))
+            return super.visit(node);
 
-        for(TsItemFct fct : item.portee.fonctions.values()){
-            if(fct.getTable().variables.containsKey(item.identif))
+        for (TsItemFct fct : table.fonctions.values()) {
+            if (fct.getTable().variables.containsKey(node.getNom()))
                 return super.visit(node);
         }
 
-        if(table.variables.containsKey(item.identif))
+        if (table.variables.containsKey(node.getNom()))
             return super.visit(node);
 
-        return null;
+        return super.visit(node);
     }
 
     @Override
     public Void visit(SaAppel node) {
-        TsItemFct item = node.tsItem;
-        if(!table.fonctions.containsKey(item.identif))
-            return null;
-        if(item.nbArgs != table.getFct(item.identif).nbArgs)
-            return null;
-        if(table.fonctions.containsKey("main") && table.getFct("main").nbArgs == 0)
+        System.out.println("4");
+        if (!table.fonctions.containsKey(node.getNom()))
+            return super.visit(node);
+        if (node.getArguments().length() != table.getFct(node.getNom()).nbArgs)
+            return super.visit(node);
+        if (table.fonctions.containsKey("main") && table.getFct("main").nbArgs == 0)
             return super.visit(node);
 
-        return null;
+        return super.visit(node);
     }
 
     @Override
     public Void visit(SaVarIndicee node) {
-        TsItemVar item = node.tsItem;
+        System.out.println("5");
 
-        if(node.getIndice() == null)
-            return null;
-
-        if(item.portee.variables.containsKey(item.identif))
+        if (node.getIndice() == null)
             return super.visit(node);
 
-        for(TsItemFct fct : item.portee.fonctions.values()){
-            if(fct.getTable().variables.containsKey(item.identif))
+        if (table.variables.containsKey(node.getNom()))
+            return super.visit(node);
+
+        for (TsItemFct fct : table.fonctions.values()) {
+            if (fct.getTable().variables.containsKey(node.getNom()))
                 return super.visit(node);
         }
 
-        if(table.variables.containsKey(item.identif))
+        if (table.variables.containsKey(node.getNom()))
             return super.visit(node);
 
-        return null;
+        return super.visit(node);
+    }
+
+    public Ts getTableGlobale() {
+        return table;
     }
 }
--- src/sa/SaDepthFirstVisitor.java
+++ src/sa/SaDepthFirstVisitor.java
@@ -63,281 +63,280 @@ package sa;
 
 
 import ts.TsItemFct;
-import ts.TsItemParam;
 import ts.TsItemVar;
 
 public class SaDepthFirstVisitor <T> implements SaVisitor <T>{
-    //   private NouvelleClasse x;
-    public void defaultIn(SaNode node)
-    {
-    }
+	//   private NouvelleClasse x;
+	public void defaultIn(SaNode node)
+	{
+	}
 
-    public void defaultOut(SaNode node)
-    {
-    }
+	public void defaultOut(SaNode node)
+	{
+	}
 
-    // P -> LDEC LDEC 
-    public T visit(SaProg node)
-    {
-	defaultIn(node);
-	if(node.getVariables() != null)
-	    node.getVariables().accept(this);
-	if(node.getFonctions() != null)
-	    node.getFonctions().accept(this);
-	defaultOut(node);
-	return null;
-    }
-    
-    // DEC -> var id taille 
-    public T visit(SaDecTab node){
+	// P -> LDEC LDEC
+	public T visit(SaProg node)
+	{
+		defaultIn(node);
+		if(node.getVariables() != null)
+			node.getVariables().accept(this);
+		if(node.getFonctions() != null)
+			node.getFonctions().accept(this);
+		defaultOut(node);
+		return null;
+	}
+
+	// DEC -> var id taille
+	public T visit(SaDecTab node){
     	/*
 	defaultIn(node);
 	defaultOut(node);
 	return null;
     	 */
 
-        TsItemVar itemVar = new TsItemVar(node.tsItem.identif,node.tsItem.getTaille());
-        return (T) itemVar;
-    }
-    
-    public T visit(SaExp node)
-    {
-	defaultIn(node);
-	defaultOut(node);
-	return null;
-    }
-    
-    // EXP -> entier
-    public T visit(SaExpInt node)
-    {
-	defaultIn(node);
-	defaultOut(node);
-	return null;
-    }
-    public T visit(SaExpVar node)
-    {
-	defaultIn(node);
-	node.getVar().accept(this);
-	defaultOut(node);
-	return null;
-    }
-    
-    public T visit(SaInstEcriture node)
-    {
-	defaultIn(node);
-	node.getArg().accept(this);
-	defaultOut(node);
-	return null;
-    }
-    
-    public T visit(SaInstTantQue node)
-    {
-	defaultIn(node);
-	node.getTest().accept(this);
-	node.getFaire().accept(this);
-	defaultOut(node);
-	return null;
-    }
-    public T visit(SaLInst node)
-    {
-	defaultIn(node);
-	node.getTete().accept(this);
-	if(node.getQueue() != null) node.getQueue().accept(this);
-	defaultOut(node);
-	return null;
-    }
+		TsItemVar itemVar = new TsItemVar(node.tsItem.identif,node.tsItem.getTaille());
+		return (T) itemVar;
+	}
 
-    // DEC -> fct id LDEC LDEC LINST 
-    public T visit(SaDecFonc node)
-    {
-	    return (T) new TsItemFct(node.tsItem.identif,node.tsItem.nbArgs, node.tsItem.getTable());
-    }
-    
-    // DEC -> var id 
-    public T visit(SaDecVar node)
-    {
-	    return (T) new TsItemVar(node.getNom(), 1);
-    }
-    
-    public T visit(SaInstAffect node)
-    {
-	defaultIn(node);
-	node.getLhs().accept(this);
-	node.getRhs().accept(this);
-	defaultOut(node);
-	return null;
-    }
-    
-    // LDEC -> DEC LDEC 
-    // LDEC -> null 
-    public T visit(SaLDec node)
-    {
-	defaultIn(node);
-	node.getTete().accept(this);
-	if(node.getQueue() != null) node.getQueue().accept(this);
-	defaultOut(node);
-	return null;
-    }
-    
-    public T visit(SaVarSimple node)
-    {
-        return (T) new TsItemVar(node.tsItem.identif,node.tsItem.getTaille());
-    }
-    
-    public T visit(SaAppel node)
-    {
-	defaultIn(node);
-	if(node.getArguments() != null) node.getArguments().accept(this);
-	defaultOut(node);
-	return null;
-    }
-    
-    public T visit(SaExpAppel node)
-    {
-	defaultIn(node);
-	node.getVal().accept(this);
-	defaultOut(node);
-	return null;
-    }
+	public T visit(SaExp node)
+	{
+		defaultIn(node);
+		defaultOut(node);
+		return null;
+	}
 
-    // EXP -> add EXP EXP
-    public T visit(SaExpAdd node)
-    {
-	defaultIn(node);
-	node.getOp1().accept(this);
-	node.getOp2().accept(this);
-	defaultOut(node);
-	return null;
-    }
+	// EXP -> entier
+	public T visit(SaExpInt node)
+	{
+		defaultIn(node);
+		defaultOut(node);
+		return null;
+	}
+	public T visit(SaExpVar node)
+	{
+		defaultIn(node);
+		node.getVar().accept(this);
+		defaultOut(node);
+		return null;
+	}
 
-    // EXP -> sub EXP EXP
-    public T visit(SaExpSub node)
-    {
-	defaultIn(node);
-	node.getOp1().accept(this);
-	node.getOp2().accept(this);
-	defaultOut(node);
-	return null;
-    }
+	public T visit(SaInstEcriture node)
+	{
+		defaultIn(node);
+		node.getArg().accept(this);
+		defaultOut(node);
+		return null;
+	}
 
-    // EXP -> mult EXP EXP
-    public T visit(SaExpMult node)
-    {
-	defaultIn(node);
-	node.getOp1().accept(this);
-	node.getOp2().accept(this);
-	defaultOut(node);
-	return null;
-    }
+	public T visit(SaInstTantQue node)
+	{
+		defaultIn(node);
+		node.getTest().accept(this);
+		node.getFaire().accept(this);
+		defaultOut(node);
+		return null;
+	}
+	public T visit(SaLInst node)
+	{
+		defaultIn(node);
+		node.getTete().accept(this);
+		if(node.getQueue() != null) node.getQueue().accept(this);
+		defaultOut(node);
+		return null;
+	}
 
-    // EXP -> div EXP EXP
-    public T visit(SaExpDiv node)
-    {
-	defaultIn(node);
-	node.getOp1().accept(this);
-	node.getOp2().accept(this);
-	defaultOut(node);
-	return null;
-    }
-    
-    // EXP -> inf EXP EXP
-    public T visit(SaExpInf node)
-    {
-	defaultIn(node);
-	node.getOp1().accept(this);
-	node.getOp2().accept(this);
-	defaultOut(node);
-	return null;
-    }
+	// DEC -> fct id LDEC LDEC LINST
+	public T visit(SaDecFonc node)
+	{
+		return (T) new TsItemFct(node.tsItem.identif,node.tsItem.nbArgs, node.tsItem.getTable(), node.tsItem.saDecFonc);
+	}
 
-    // EXP -> eq EXP EXP
-    public T visit(SaExpEqual node)
-    {
-	defaultIn(node);
-	node.getOp1().accept(this);
-	node.getOp2().accept(this);
-	defaultOut(node);
-	return null;
-    }
+	// DEC -> var id
+	public T visit(SaDecVar node)
+	{
+		return (T) new TsItemVar(node.getNom(), 1);
+	}
 
-    // EXP -> and EXP EXP
-    public T visit(SaExpAnd node)
-    {
-	defaultIn(node);
-	node.getOp1().accept(this);
-	node.getOp2().accept(this);
-	defaultOut(node);
-	return null;
-    }
-    
+	public T visit(SaInstAffect node)
+	{
+		defaultIn(node);
+		node.getLhs().accept(this);
+		node.getRhs().accept(this);
+		defaultOut(node);
+		return null;
+	}
 
-    // EXP -> or EXP EXP
-    public T visit(SaExpOr node)
-    {
-	defaultIn(node);
-	node.getOp1().accept(this);
-	node.getOp2().accept(this);
-	defaultOut(node);
-	return null;
-    }
+	// LDEC -> DEC LDEC
+	// LDEC -> null
+	public T visit(SaLDec node)
+	{
+		defaultIn(node);
+		node.getTete().accept(this);
+		if(node.getQueue() != null) node.getQueue().accept(this);
+		defaultOut(node);
+		return null;
+	}
 
-    // EXP -> not EXP
-    public T visit(SaExpNot node)
-    {
-	defaultIn(node);
-	node.getOp1().accept(this);
-	defaultOut(node);
-	return null;
-    }
+	public T visit(SaVarSimple node)
+	{
+		return (T) new TsItemVar(node.tsItem.identif,node.tsItem.getTaille());
+	}
+
+	public T visit(SaAppel node)
+	{
+		defaultIn(node);
+		if(node.getArguments() != null) node.getArguments().accept(this);
+		defaultOut(node);
+		return null;
+	}
+
+	public T visit(SaExpAppel node)
+	{
+		defaultIn(node);
+		node.getVal().accept(this);
+		defaultOut(node);
+		return null;
+	}
+
+	// EXP -> add EXP EXP
+	public T visit(SaExpAdd node)
+	{
+		defaultIn(node);
+		node.getOp1().accept(this);
+		node.getOp2().accept(this);
+		defaultOut(node);
+		return null;
+	}
+
+	// EXP -> sub EXP EXP
+	public T visit(SaExpSub node)
+	{
+		defaultIn(node);
+		node.getOp1().accept(this);
+		node.getOp2().accept(this);
+		defaultOut(node);
+		return null;
+	}
+
+	// EXP -> mult EXP EXP
+	public T visit(SaExpMult node)
+	{
+		defaultIn(node);
+		node.getOp1().accept(this);
+		node.getOp2().accept(this);
+		defaultOut(node);
+		return null;
+	}
+
+	// EXP -> div EXP EXP
+	public T visit(SaExpDiv node)
+	{
+		defaultIn(node);
+		node.getOp1().accept(this);
+		node.getOp2().accept(this);
+		defaultOut(node);
+		return null;
+	}
+
+	// EXP -> inf EXP EXP
+	public T visit(SaExpInf node)
+	{
+		defaultIn(node);
+		node.getOp1().accept(this);
+		node.getOp2().accept(this);
+		defaultOut(node);
+		return null;
+	}
+
+	// EXP -> eq EXP EXP
+	public T visit(SaExpEqual node)
+	{
+		defaultIn(node);
+		node.getOp1().accept(this);
+		node.getOp2().accept(this);
+		defaultOut(node);
+		return null;
+	}
+
+	// EXP -> and EXP EXP
+	public T visit(SaExpAnd node)
+	{
+		defaultIn(node);
+		node.getOp1().accept(this);
+		node.getOp2().accept(this);
+		defaultOut(node);
+		return null;
+	}
 
 
-    public T visit(SaExpLire node)
-    {
-	defaultIn(node);
-	defaultOut(node);
-	return null;
-    }
+	// EXP -> or EXP EXP
+	public T visit(SaExpOr node)
+	{
+		defaultIn(node);
+		node.getOp1().accept(this);
+		node.getOp2().accept(this);
+		defaultOut(node);
+		return null;
+	}
 
-    public T visit(SaInstBloc node)
-    {
-	defaultIn(node);
-	node.getVal().accept(this);
-	defaultOut(node);
-	return null;
-    }
-    
-    public T visit(SaInstSi node)
-    {
-	defaultIn(node);
-	node.getTest().accept(this);
-	node.getAlors().accept(this);
-	if(node.getSinon() != null) node.getSinon().accept(this);
-	defaultOut(node);
-	return null;
-    }
+	// EXP -> not EXP
+	public T visit(SaExpNot node)
+	{
+		defaultIn(node);
+		node.getOp1().accept(this);
+		defaultOut(node);
+		return null;
+	}
 
-// INST -> ret EXP 
-    public T visit(SaInstRetour node)
-    {
-	defaultIn(node);
-	node.getVal().accept(this);
-	defaultOut(node);
-	return null;
-    }
 
-    
-    public T visit(SaLExp node)
-    {
-	defaultIn(node);
-	node.getTete().accept(this);
-	if(node.getQueue() != null)
-	    node.getQueue().accept(this);
-	defaultOut(node);
-	return null;
-    }
-    public T visit(SaVarIndicee node)
-    {
-	    return (T) new TsItemVar(node.tsItem.identif,node.tsItem.getTaille());
-    }
-    
+	public T visit(SaExpLire node)
+	{
+		defaultIn(node);
+		defaultOut(node);
+		return null;
+	}
+
+	public T visit(SaInstBloc node)
+	{
+		defaultIn(node);
+		node.getVal().accept(this);
+		defaultOut(node);
+		return null;
+	}
+
+	public T visit(SaInstSi node)
+	{
+		defaultIn(node);
+		node.getTest().accept(this);
+		node.getAlors().accept(this);
+		if(node.getSinon() != null) node.getSinon().accept(this);
+		defaultOut(node);
+		return null;
+	}
+
+	// INST -> ret EXP
+	public T visit(SaInstRetour node)
+	{
+		defaultIn(node);
+		node.getVal().accept(this);
+		defaultOut(node);
+		return null;
+	}
+
+
+	public T visit(SaLExp node)
+	{
+		defaultIn(node);
+		node.getTete().accept(this);
+		if(node.getQueue() != null)
+			node.getQueue().accept(this);
+		defaultOut(node);
+		return null;
+	}
+	public T visit(SaVarIndicee node)
+	{
+		return (T) new TsItemVar(node.tsItem.identif,node.tsItem.getTaille());
+	}
+
 }
